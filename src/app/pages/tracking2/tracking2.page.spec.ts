97531411c77b31eac8d46d2cddb95b5df3e7cf41
DESKTOP-4EH5F8F\jcgv6 <josecgomezvazquez@gmail.com>
2021-04-23 15:03:34 -0500

    Created tracking2 and tracking3

---
+++ src/app/pages/tracking2/tracking2.page.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { Tracking2Page } from './tracking2.page';
+
+describe('Tracking2Page', () => {
+  let component: Tracking2Page;
+  let fixture: ComponentFixture<Tracking2Page>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ Tracking2Page ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Tracking2Page);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
