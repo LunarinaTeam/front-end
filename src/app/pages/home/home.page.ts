bda4eac7fa1d58214e2f56869c0fb79759fe94fc
Kassy <kssy.dz@gmail.com>
2021-04-23 15:48:40 -0500

    adding homepage from wireframe

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
+  
 })
 export class HomePage implements OnInit {
 
-  constructor(){}
+  today=new Date();
+  
+
+
 
   ngOnInit() {
   }
-
-
+  
 }
