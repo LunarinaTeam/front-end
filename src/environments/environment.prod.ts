b08f9944af587e37929b30017742681a35ff8c56
Tiffany <Tiffanytabour@gmail.com>
2021-05-07 11:45:13 -0500

    firebase configuration for login page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
   production: true
 };
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCSvMjCt07hOHmpVZyk17P6CQWwy3Spnx4",
+  authDomain: "events-search-qohi.firebaseapp.com",
+  projectId: "events-search-qohi",
+  storageBucket: "events-search-qohi.appspot.com",
+  messagingSenderId: "83290592911",
+  appId: "1:83290592911:web:2ffd649f5be23d536982fa",
+  measurementId: "G-RL6F99LR0G"
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,15 @@ export const environment = {
   production: false
 };
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyCSvMjCt07hOHmpVZyk17P6CQWwy3Spnx4",
+  authDomain: "events-search-qohi.firebaseapp.com",
+  projectId: "events-search-qohi",
+  storageBucket: "events-search-qohi.appspot.com",
+  messagingSenderId: "83290592911",
+  appId: "1:83290592911:web:2ffd649f5be23d536982fa",
+  measurementId: "G-RL6F99LR0G"
+};
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
