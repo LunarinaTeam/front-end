dbc77feecdade28aaa370596ea039ab3ba004ad1
DESKTOP-4EH5F8F\jcgv6 <josecgomezvazquez@gmail.com>
2021-05-08 13:44:12 -0500

    Finished tracking pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tracking1',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,16 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
+import {FormGroup, FormBuilder, Validators, FormControl} from '@angular/forms'; 
+import {AuthService} from 'src/app/services/auth.service'; 
 
 @Component({
-  selector: 'app-signup',
+  selector: 'app-test-slide',
   templateUrl: './signup.page.html',
   styleUrls: ['./signup.page.scss'],
 })
-export class SignupPage implements OnInit {
+export class TestSlidePage implements OnInit {
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400,
+  };
 
-  constructor(public router:Router) { }
+  validationMessages = {
+    email: [
+      {type: 'required',message:"Enter your email address"},
+      {type:"pattern", meesage:"Please fix the formatting for your email"}
+    ],
+    password: [
+      {type: "required", message: "Enter your new password here"},
+      {type:"minlength", message: "Your password must be at least 8 characters"}
+    ]
+ }
+
+  ValidationFormUser : FormGroup;
+  constructor(public router:Router,public formBuilder : FormBuilder, public authService: AuthService) { }
 
   ngOnInit() {
+    this.ValidationFormUser = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('',Validators.compose([
+        Validators.required,
+        Validators.minLength(8),
+      ]))
+    })
+  }
+  routeToSignUp(){
   }
 
   RedirectToLoginPage()
