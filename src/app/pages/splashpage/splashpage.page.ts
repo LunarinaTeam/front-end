ff31b8c5f97f4d2722356e14ceab63442883b618
Tiffany <Tiffanytabour@gmail.com>
2021-05-09 19:57:12 -0500

    Fixed font error

--- src/app/pages/splashpage/splashpage.page.ts
+++ src/app/pages/splashpage/splashpage.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-splashpage',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SplashpagePage implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router ) { }
 
   ngOnInit() {
+  }
+  RedirectToSignUp(){
+    this.router.navigateByUrl('/signup');
   }
 
 }
